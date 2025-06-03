505d48c6029ac9560ff035234fb51b58722527ee
Kate <s30046@pjwstk.edu.pl>
2025-06-03 17:56:35 +0200

    Generate models with EF

---
+++ APBD-cw10-s30046/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APBD_cw10_s30046.Models;
+
+public partial class Country
+{
+    public int IdCountry { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Trip> IdTrips { get; set; } = new List<Trip>();
+}
